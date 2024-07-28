fdbbc0a89326eb224c4aea6dcf1f4e97a39bfde7
Huangxy-Minel <huangxy_minel@foxmail.com>
2024-07-28 23:15:29 +0800

    init code base

---
+++ lib/channel/nicc_channel.h
@@ -0,0 +1,25 @@
+/**
+ * @file comm.h
+ * @brief General definitions for all communcation channel types. 
+ */
+
+#pragma once
+#include "common.h"
+
+namespace nicc {
+class Channel {
+/**
+ * ----------------------Parameters in channel level----------------------
+ */ 
+/**
+ * ----------------------Channel internal structures----------------------
+ */ 
+/**
+ * ----------------------Channel methods----------------------
+ */ 
+public:
+    Channel();
+    ~Channel();
+};
+} 
+
