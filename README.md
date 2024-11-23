17acd6040176798bad1776d95d6b946584901b20
Huangxy-Minel <huangxy_minel@foxmail.com>
2024-11-23 11:40:50 +0000

    [Features] parse app dag: parsing App DAG

--- README.md
+++ README.md
@@ -5,5 +5,5 @@ A compiler for merging multi-users' BlueField3 codes with pipeline parallelism.
 ## Build
 
 ```bash
-sudo apt-get install binutils-dev
+sudo apt-get install binutils-dev nlohmann-json3-dev
 ```
--- lib/common.h
+++ lib/common.h
@@ -179,4 +179,14 @@ static inline void rt_assert(bool condition) {
     exit(-1);
   }
 }
+
+/**
+ * ----------------------Print related----------------------
+ */ 
+const std::string RESET = "\033[0m";
+const std::string RED = "\033[31m";
+const std::string GREEN = "\033[32m";
+const std::string YELLOW = "\033[33m";
+const std::string BLUE = "\033[34m";
+
 } // namespace nicc
