84453869b392b969a1ed55529dad8dfb35d68703
Huangxy-Minel <huangxy_minel@foxmail.com>
2025-01-20 05:39:19 +0000

    [Tmp]: complete SoC wrapper code structure

--- lib/wrapper/soc/soc_wrapper.h
+++ lib/wrapper/soc/soc_wrapper.h
@@ -68,7 +68,44 @@ class SoCWrapper {
     nicc_retval_t __run();
 
     /* ========================SoC Datapath ========================*/
-    
+
+    /**
+     * \brief Post receive wrs to the NIC, and update the recv head
+     * \param RDMA_SoC_QP *qp, the QP for receiving packets
+     * \return the number of packets received
+     */
+    size_t __post_recvs(RDMA_SoC_QP *qp);
+
+    /**
+     * \brief Receive packets from the NIC and put them into the dispatcher rx queue.
+     * \param RDMA_SoC_QP *qp, the QP for receiving packets
+     * \return the number of packets received
+     */
+    size_t __rx_burst(RDMA_SoC_QP *qp);
+
+    /**
+     * \brief Dispatch packets from the dispatcher rx queue to the worker rx queue 
+     * based on packet UDP field. Workspace will be blocked until all packets are
+     * dispatched.
+     * \param RDMA_SoC_QP *qp, the QP for receiving packets
+     * \return the number of packets dispatched
+     */
+    size_t __dispatch_rx_pkts(RDMA_SoC_QP *qp);
+
+    /**
+     * \brief Flush the dispatcher tx queue to the NIC. Dispatcher will be blocked
+     * until all packets are sent
+     * \param RDMA_SoC_QP *qp, the QP for sending packets
+     * \return the number of packets sent
+     */
+    size_t __tx_flush(RDMA_SoC_QP *qp);
+
+    /**
+     * \brief Iterate all worker queues assigned to this dispatcher, and collect packets from them.
+     * \param RDMA_SoC_QP *qp, the QP for sending packets
+     * \return the number of packets collected
+     */
+    size_t __collect_tx_pkts(RDMA_SoC_QP *qp);
 };
 
 
